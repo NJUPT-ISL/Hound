2c7865942d5207319261e47906e47c3ba74ff3bf
Funky <708863861@qq.com>
2019-04-07 17:21:47 +0800

    Add log func

--- agent/main.go
+++ agent/main.go
@@ -1,10 +1,23 @@
 package main
 import  (
 	"./routers"
+	"github.com/gin-gonic/gin"
+	"io"
+	"os"
 )
 
 
 func main() {
+	// Disable Debug mode
+	gin.SetMode(gin.ReleaseMode)
+	// Enable Logs
+	gin.DisableConsoleColor()
+	f, err := os.Create("log/agent.log")
+	if err != nil{
+		panic(err)
+	}
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
 	r := routers.InitRouter()
 	_ = r.RunTLS(":8080","pem/server.crt","pem/server.key") // listen and serve on 0.0.0.0:8080
 }
